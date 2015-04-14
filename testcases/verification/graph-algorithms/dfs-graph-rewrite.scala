bed21f0c247418d77c76f4b3ed6d3168b9e6bc6d
ravi <ravi.kandhadai@epfl.ch>
2015-04-14 18:30:03 +0200

    updating dfs implementation

--- testcases/verification/graph-algorithms/dfs-graph-rewrite.scala
+++ testcases/verification/graph-algorithms/dfs-graph-rewrite.scala
@@ -1,26 +1,20 @@
 import leon.annotation._
 import Graph._
 
-object GraphDFS {  
+object GraphDFS {
 
   def dfs(g: Graph, stack: NodeList): (NodeList, BigInt) = {
-    require(isDistinct(g.nodes) && isDistinct(stack) &&
-      contents(stack).subsetOf(contents(g.nodes)))      
+    require(validGraph(g) && 
+        isDistinct(stack) && contents(stack).subsetOf(contents(g.nodes)))        
     stack match {
       case Nil() =>
         (Nil(), 1)
       case Cons(n, tail) =>
-        val succs = getSuccs(g, n) //constant time       
-        succs match {
-          case Nil() =>
-            val (reach, recTime) = dfs(g, tail)
-            (reach, recTime + 1)
-          case _ => 
-            val newgraph = removeSuccEdges(g, n) // constant time
-            val (newstack, appendTime) = append(succs, tail) //O(succs) time        
-            val (reach, recTime) = dfs(newgraph, newstack)
-            (reach, appendTime + recTime + 1)
-        }        
+        val succs = getSuccs(g, n) //constant time               
+        val newgraph = removeSuccEdges(g, n) // constant time
+        val (newstack, appendTime) = append(succs, tail) //O(succs) time        
+        val (reach, recTime) = dfs(newgraph, newstack)
+        (reach, appendTime + recTime + 1)
     }
-  } ensuring (res => res._2 <= 3 * edgeSize(g) + size(stack) + 1)
+  } ensuring (res => res._2 <= 6 * edgeSize(g) + 2 * size(stack) + 1)
 }
