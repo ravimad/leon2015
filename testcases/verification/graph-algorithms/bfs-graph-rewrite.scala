e689938beddfc5f34e0cf4c42b316d3a395e4a1e
ravi <ravi.kandhadai@epfl.ch>
2015-04-14 18:49:58 +0200

    Adding bfs

---
+++ testcases/verification/graph-algorithms/bfs-graph-rewrite.scala
@@ -0,0 +1,40 @@
+import leon.annotation._
+import Graph._
+
+object GraphBFS {
+    
+  /**
+   * Assuming an imperative implementation that runs O(1)
+   * time for enqueue and dequeue
+   */
+  def appendQueue(q: NodeList, elems: NodeList) : (NodeList, BigInt) = { 
+    require(isDistinct(q) && isDistinct(elems))
+    //using list append, but ideally this will have smaller running time
+    val (res, t) = append(q, elems)
+    (res, size(elems) + 1)
+  } 
+  
+  def bfs(g: Graph, queue: NodeList): (NodeList, BigInt) = {
+    require(validGraph(g) &&
+        isDistinct(queue) && contents(queue).subsetOf(contents(g.nodes)))        
+    queue match {
+      case Nil() =>
+        (Nil(), 1)
+      case Cons(n, tail) =>
+        val succs = getSuccs(g, n) //constant time       
+        val newgraph = removeSuccEdges(g, n)
+        val (newqueue, appendTime) = appendQueue(tail, succs) //time O(succs)
+        val (reach, recTime) = bfs(newgraph, newqueue)
+        (reach, appendTime + recTime + 1)
+    }
+  } ensuring (res => res._2 <= 6 * edgeSize(g) + 2 * size(queue) + 1)
+  
+  def bfsVertex(g: Graph, v : Node) : (NodeList, BigInt) = {
+    require(validGraph(g) && contents(g.nodes).contains(v))
+    
+    bfs(g, Cons(v, Nil()))
+    
+  } ensuring (res => res._2 <= 6 * edgeSize(g) + 3)
+  
+  
+}
---
+++ testcases/verification/graph-algorithms/graph.scala
@@ -0,0 +1,158 @@
+import scala.collection.immutable._
+import leon.annotation._
+
+object Graph {
+
+  sealed abstract class NodeList
+  case class Cons(node: Node, tail: NodeList) extends NodeList
+  case class Nil() extends NodeList
+
+  case class Node(id: BigInt)
+  case class Edge(src: Node, dst: Node)
+  case class Graph(nodes: NodeList, adjlist: Map[Node, NodeList])
+
+  //list to set
+  def contents(nl: NodeList): Set[Node] = {
+    nl match {
+      case Nil() =>
+        Set[Node]()
+      case Cons(n, tail) =>
+        Set(n) ++ contents(tail)
+    }
+  }  
+  
+  //graph invariants    
+  def nodeInv(g: Graph, n: Node) : Boolean = {
+    !contents(g.nodes).contains(n) || (g.adjlist.contains(n) && {
+      isDistinct(g.adjlist(n)) && //distinct successor list
+        !contents(g.adjlist(n)).contains(n) && //no self loops (for now)
+        contents(g.adjlist(n)).subsetOf(contents(g.nodes)) //succs contained in node set        
+    })        
+  }
+  
+  /**
+   * For every node the graph invariant holds
+   */
+  def graphInvRec(g: Graph, nodes : NodeList) : Boolean = {
+    nodes match {
+      case Nil() => 
+        true
+      case Cons(v, tail) =>
+        nodeInv(g, v) && graphInvRec(g, tail) 
+    }
+  }
+  
+  def validGraph(g: Graph) : Boolean = {
+    isDistinct(g.nodes) && graphInvRec(g, g.nodes)  
+  }   
+  
+  /**
+   * This sort of encapsulates an axiom that when pre holds
+   * post also holds
+   */
+  def getSuccs(g: Graph, node: Node): NodeList = {
+    require(contents(g.nodes).contains(node) && validGraph(g))
+    
+    g.adjlist(node)
+
+  } ensuring (res => nodeInv(g, node))
+  
+   //graph size metrics
+  def size(nl: NodeList): BigInt = {
+    nl match {
+      case Nil() =>
+        0
+      case Cons(n, tail) =>
+        1 + size(tail)
+    }
+  } ensuring (_ >= 0)
+
+  def nodeSize(g: Graph): BigInt = {
+    size(g.nodes)
+  }
+
+  def sumSuccs(g: Graph, nodes: NodeList): BigInt = {
+    require(validGraph(g) && contents(nodes).subsetOf(contents(g.nodes)))    
+    nodes match {
+      case Nil() =>
+        0
+      case Cons(n, tail) =>
+        size(getSuccs(g, n)) + sumSuccs(g, tail)
+    }
+  } ensuring (_ >= 0)
+
+  def edgeSize(g: Graph): BigInt = {
+    require(validGraph(g))
+    
+    sumSuccs(g, g.nodes)
+  }  
+
+  def graphSize(g: Graph): BigInt = {
+    require(validGraph(g))
+    
+    nodeSize(g) + edgeSize(g)
+  }
+
+  def contains(nl: NodeList, key: Node): Boolean = {
+    nl match {
+      case Nil() =>
+        false
+      case Cons(n, tail) =>
+        if (n == key)
+          true
+        else contains(tail, key)
+    }
+  } ensuring (res => (res && Set(key).subsetOf(contents(nl))) ||
+    (!res && !Set(key).subsetOf(contents(nl))))
+
+  def isDistinct(nl: NodeList): Boolean = {
+    nl match {
+      case Nil() =>
+        true
+      case Cons(n, tail) =>
+        !contains(tail, n) && isDistinct(tail)
+    }
+  }
+
+  def append(l1: NodeList, l2: NodeList): (NodeList, BigInt) = {
+    require(isDistinct(l1) && isDistinct(l2))
+    l1 match {
+      case Nil() =>
+        (l2, 1)
+      case Cons(n, tail) =>
+        val (res, t) = append(tail, l2)
+        if (!contains(res, n))
+          (Cons(n, res), t + 1)
+        else
+          (res, t + 1)
+    }
+  } ensuring (res => isDistinct(res._1) &&    
+    contents(res._1) == contents(l1) ++ contents(l2) &&
+    size(res._1) <= size(l1) + size(l2) &&
+    res._2 <= size(l1) + 1)
+
+  /**
+   * Requires that 'key' is contained in 'nodes'.
+   */
+  def remove(nodes: NodeList, key: Node): NodeList = {
+    require(Set(key).subsetOf(contents(nodes)) && isDistinct(nodes))
+    nodes match {
+      case Nil() =>
+        Nil()
+      case Cons(n, tail) if n == key =>
+        tail
+      case Cons(n, tail) =>
+        Cons(n, remove(tail, key))
+    }
+  } ensuring (res => (contents(res) == contents(nodes) -- Set(key)) &&
+    isDistinct(res) &&
+    (size(res) == size(nodes) - 1))
+
+  def removeSuccEdges(g: Graph, n: Node): Graph = {
+    require(validGraph(g) && contents(g.nodes).contains(n))
+
+    Graph(g.nodes, g.adjlist.updated(n, Nil()))
+
+  } ensuring (res => validGraph(res) && 
+      edgeSize(res) == edgeSize(g) - size(getSuccs(g, n)))
+}
