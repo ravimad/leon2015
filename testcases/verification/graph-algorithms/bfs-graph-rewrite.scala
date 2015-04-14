aace920ca33abc1fa86198781a0f21fc45c7df87
ravi <ravi.kandhadai@epfl.ch>
2015-04-14 19:30:07 +0200

    updating bfs to a more precise bound

--- testcases/verification/graph-algorithms/bfs-graph-rewrite.scala
+++ testcases/verification/graph-algorithms/bfs-graph-rewrite.scala
@@ -12,29 +12,43 @@ object GraphBFS {
     //using list append, but ideally this will have smaller running time
     val (res, t) = append(q, elems)
     (res, size(elems) + 1)
-  } 
+  }
   
-  def bfs(g: Graph, queue: NodeList): (NodeList, BigInt) = {
+  def succsSize(g: Graph, nl : NodeList) : BigInt = {
+    require(validGraph(g) && contents(nl).subsetOf(contents(g.nodes)) )
+    
+    nl match {
+      case Nil() =>
+        0
+      case Cons(n, tail) =>
+        size(getSuccs(g, n)) + succsSize(g, tail)
+    } 
+  }
+  
+  def bfs(g: Graph, queue: NodeList): (NodeList, Graph, BigInt) = {
     require(validGraph(g) &&
         isDistinct(queue) && contents(queue).subsetOf(contents(g.nodes)))        
     queue match {
       case Nil() =>
-        (Nil(), 1)
+        (Nil(), g, 1)
       case Cons(n, tail) =>
         val succs = getSuccs(g, n) //constant time       
         val newgraph = removeSuccEdges(g, n)
         val (newqueue, appendTime) = appendQueue(tail, succs) //time O(succs)
-        val (reach, recTime) = bfs(newgraph, newqueue)
-        (Cons(n, reach), appendTime + recTime + 1)
+        val (reach, finalg, recTime) = bfs(newgraph, newqueue)
+        (Cons(n, reach), finalg, appendTime + recTime + 1)
     }
-  } ensuring (res => res._2 <= 6 * edgeSize(g) + 2 * size(queue) + 1)
+  } ensuring (res => contents(res._1).subsetOf(contents(g.nodes)) && 
+		  edgeSize(res._2) == edgeSize(g) - succsSize(g, res._1))
   
-  def bfsVertex(g: Graph, v : Node) : (NodeList, BigInt) = {
+  //ensuring (res => res._2 <= 6 * edgeSize(g) + 2 * size(queue) + 1)
+  
+  /*def bfsVertex(g: Graph, v : Node) : (NodeList, BigInt) = {
     require(validGraph(g) && contents(g.nodes).contains(v))
     
     bfs(g, Cons(v, Nil()))
     
-  } ensuring (res => res._2 <= 6 * edgeSize(g) + 3)
+  } *///ensuring (res => res._2 <= 6 * edgeSize(g) + 3)
   
   
 }
